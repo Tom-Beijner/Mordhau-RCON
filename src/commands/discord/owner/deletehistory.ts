301b9808c362d63b5d4e90c15230852731c92cf1
Tom Beijner <tom.beijner@gmail.com>
2021-06-25 11:28:23 +0200

    Add deletehistory and deletepunishment command

---
+++ src/commands/discord/owner/deletehistory.ts
@@ -0,0 +1,236 @@
+import flatMap from "array.prototype.flatmap";
+import { addMinutes, formatDistanceToNow } from "date-fns";
+import pluralize from "pluralize";
+import {
+    ApplicationCommandPermissionType,
+    CommandContext,
+    CommandOptionType,
+    SlashCreator,
+} from "slash-create";
+import config from "../../../config.json";
+import { ComponentConfirmation } from "../../../services/Discord";
+import { LookupPlayer } from "../../../services/PlayFab";
+import SlashCommand from "../../../structures/SlashCommand";
+import Watchdog from "../../../structures/Watchdog";
+import { hastebin } from "../../../utils/Hastebin";
+import logger from "../../../utils/logger";
+import { outputPlayerIDs, parsePlayerID } from "../../../utils/PlayerID";
+
+export default class DeleteHistory extends SlashCommand {
+    constructor(creator: SlashCreator, bot: Watchdog, commandName: string) {
+        super(creator, bot, {
+            name: commandName,
+            description: "Delete history of a player or an admin",
+            options: [
+                {
+                    name: "type",
+                    description: "Type of player to clear",
+                    required: true,
+                    type: CommandOptionType.STRING,
+                    choices: [
+                        {
+                            name: "Player",
+                            value: "Player",
+                        },
+                        {
+                            name: "Admin",
+                            value: "Admin",
+                        },
+                    ],
+                },
+                {
+                    name: "player",
+                    description: "PlayFab ID or name of the player",
+                    required: true,
+                    type: CommandOptionType.STRING,
+                },
+            ],
+            defaultPermission: false,
+            permissions: {
+                [config.discord.guildId]: flatMap(
+                    config.discord.roles.filter((role) =>
+                        role.commands.includes(commandName)
+                    ),
+                    (role) =>
+                        role.Ids.map((id) => ({
+                            type: ApplicationCommandPermissionType.ROLE,
+                            id,
+                            permission: true,
+                        }))
+                ),
+            },
+        });
+    }
+
+    async run(ctx: CommandContext) {
+        const options = {
+            type: ctx.options.type.toLowerCase() as string,
+            player: ctx.options.player as string,
+        };
+
+        const ingamePlayer = await this.bot.rcon.getIngamePlayer(
+            ctx.options.player as string
+        );
+        const player =
+            this.bot.cachedPlayers.get(ingamePlayer?.id || options.player) ||
+            (await LookupPlayer(ingamePlayer?.id || options.player));
+
+        if (!player?.id) {
+            return await ctx.send(`Invalid player provided`);
+        }
+
+        try {
+            const playerHistory = await this.bot.database.getPlayerHistory(
+                [player.ids.playFabID, player.ids.steamID],
+                options.type === "admin"
+            );
+
+            let pastOffenses: string;
+            let totalDuration = 0;
+
+            if (!playerHistory.history.length) pastOffenses = "None";
+            else {
+                pastOffenses = "------------------";
+
+                for (let i = 0; i < playerHistory.history.length; i++) {
+                    const offense: string[] = [];
+                    const h = playerHistory.history[i];
+                    const type = h.type;
+                    const admin = h.admin;
+                    const date = new Date(h.date);
+
+                    let historyDuration: string;
+                    if (!h.duration) historyDuration = "PERMANENT";
+                    else {
+                        historyDuration = pluralize("minute", h.duration, true);
+                        totalDuration += h.duration;
+                    }
+
+                    offense.push(
+                        [
+                            `\nID: ${i + 1}`,
+                            `Type: ${type}`,
+                            type.includes("GLOBAL")
+                                ? undefined
+                                : `Server: ${h.server}`,
+                            `Platform: ${parsePlayerID(h.id).platform}`,
+                            options.type === "admin"
+                                ? `Player: ${h.player} (${outputPlayerIDs(
+                                      h.ids.length
+                                          ? h.ids
+                                          : [
+                                                {
+                                                    platform: parsePlayerID(
+                                                        h.id
+                                                    ).platform,
+                                                    id: h.id,
+                                                },
+                                            ]
+                                  )})`
+                                : undefined,
+                            `Date: ${date.toDateString()} (${formatDistanceToNow(
+                                date,
+                                { addSuffix: true }
+                            )})`,
+                            `Admin: ${admin}`,
+                            `Offense: ${h.reason || "None given"}`,
+                            [
+                                "BAN",
+                                "MUTE",
+                                "GLOBAL BAN",
+                                "GLOBAL MUTE",
+                            ].includes(type)
+                                ? `Duration: ${historyDuration} ${
+                                      h.duration
+                                          ? `(Un${
+                                                type === "BAN"
+                                                    ? "banned"
+                                                    : "muted"
+                                            } ${formatDistanceToNow(
+                                                addMinutes(date, h.duration),
+                                                { addSuffix: true }
+                                            )})`
+                                          : ""
+                                  }`
+                                : undefined,
+                            `------------------`,
+                        ]
+                            .filter((line) => typeof line !== "undefined")
+                            .join("\n")
+                    );
+
+                    pastOffenses += offense.join("\n");
+                }
+
+                if (pastOffenses.length < 1025)
+                    pastOffenses = `\`\`\`${pastOffenses}\`\`\``;
+            }
+
+            if (pastOffenses.length > 1024)
+                pastOffenses = `The output was too long, but was uploaded to [hastebin](${await hastebin(
+                    pastOffenses
+                )})`;
+
+            ComponentConfirmation(
+                ctx,
+                {
+                    embeds: [
+                        {
+                            description: `Are you sure you want to delete \`${
+                                options.type
+                            }\` history of ${player.name} (${outputPlayerIDs(
+                                player.ids,
+                                true
+                            )})?`,
+                            fields: [
+                                {
+                                    name:
+                                        options.type === "player"
+                                            ? `Previous Offenses (${playerHistory.history.length})`
+                                            : `Punishments Given (${playerHistory.history.length})`,
+                                    value: pastOffenses,
+                                },
+                            ],
+                            color: 15158332,
+                        },
+                    ],
+                },
+                async (btnCtx) => {
+                    if (ctx.user.id !== btnCtx.user.id) return;
+                    await this.bot.database.deletePlayerHistory(
+                        [options.player],
+                        options.type === "admin"
+                    );
+
+                    logger.info(
+                        "Command",
+                        `${ctx.member.displayName}#${
+                            ctx.member.user.discriminator
+                        } cleared ${options.type} history of ${
+                            player.name
+                        } (${outputPlayerIDs(player.ids, true)})`
+                    );
+
+                    await btnCtx.editParent({
+                        embeds: [
+                            {
+                                description: `Cleared \`${
+                                    options.type
+                                }\` history of ${
+                                    player.name
+                                } (${outputPlayerIDs(player.ids, true)})\n`,
+                            },
+                        ],
+                        components: [],
+                    });
+                }
+            );
+        } catch (error) {
+            await ctx.send({
+                content: `An error occured while performing the command (${
+                    error.message || error
+                })`,
+            });
+        }
+    }
+}
---
+++ src/commands/discord/owner/deletepunishment.ts
@@ -0,0 +1,210 @@
+import flatMap from "array.prototype.flatmap";
+import { addMinutes, formatDistanceToNow } from "date-fns";
+import pluralize from "pluralize";
+import {
+    ApplicationCommandPermissionType,
+    CommandContext,
+    CommandOptionType,
+    SlashCreator,
+} from "slash-create";
+import config from "../../../config.json";
+import { ComponentConfirmation } from "../../../services/Discord";
+import { LookupPlayer } from "../../../services/PlayFab";
+import SlashCommand from "../../../structures/SlashCommand";
+import Watchdog from "../../../structures/Watchdog";
+import { outputPlayerIDs, parsePlayerID } from "../../../utils/PlayerID";
+
+export default class DeletePunishment extends SlashCommand {
+    constructor(creator: SlashCreator, bot: Watchdog, commandName: string) {
+        super(creator, bot, {
+            name: commandName,
+            description: "Clear history or punishment of a player or an admin",
+            options: [
+                {
+                    name: "type",
+                    description: "Type of player to clear",
+                    required: true,
+                    type: CommandOptionType.STRING,
+                    choices: [
+                        {
+                            name: "Player",
+                            value: "Player",
+                        },
+                        {
+                            name: "Admin",
+                            value: "Admin",
+                        },
+                    ],
+                },
+                {
+                    name: "punishment_id",
+                    description: "The ID of the punishment to clear",
+                    required: true,
+                    type: CommandOptionType.INTEGER,
+                },
+                {
+                    name: "player",
+                    description: "PlayFab ID or name of the player",
+                    required: true,
+                    type: CommandOptionType.STRING,
+                },
+            ],
+            defaultPermission: false,
+            permissions: {
+                [config.discord.guildId]: flatMap(
+                    config.discord.roles.filter((role) =>
+                        role.commands.includes(commandName)
+                    ),
+                    (role) =>
+                        role.Ids.map((id) => ({
+                            type: ApplicationCommandPermissionType.ROLE,
+                            id,
+                            permission: true,
+                        }))
+                ),
+            },
+        });
+    }
+
+    async run(ctx: CommandContext) {
+        const options = {
+            type: ctx.options.type.toLowerCase() as string,
+            player: ctx.options.player as string,
+            punishmentID: ctx.options.punishment_id as number,
+        };
+
+        const ingamePlayer = await this.bot.rcon.getIngamePlayer(
+            ctx.options.player as string
+        );
+        const player =
+            this.bot.cachedPlayers.get(ingamePlayer?.id || options.player) ||
+            (await LookupPlayer(ingamePlayer?.id || options.player));
+
+        if (!player?.id) {
+            return await ctx.send(`Invalid player provided`);
+        }
+
+        try {
+            if (options.punishmentID < 1)
+                return "Punishment ID must be at least 1";
+
+            const {
+                punishment: {
+                    type,
+                    server,
+                    date,
+                    admin,
+                    duration,
+                    reason,
+                    id: punishedPlayerID,
+                },
+            } = await this.bot.database.getPlayerPunishment(
+                [player.ids.playFabID, player.ids.steamID],
+                options.punishmentID,
+                options.type === "admin"
+            );
+
+            const punishedPlayer =
+                this.bot.cachedPlayers.get(punishedPlayerID) ||
+                (await LookupPlayer(punishedPlayerID));
+
+            ComponentConfirmation(
+                ctx,
+                {
+                    embeds: [
+                        {
+                            description: `Are you sure you want to delete \`${
+                                options.type
+                            }\` punishment of ${player.name} (${outputPlayerIDs(
+                                player.ids,
+                                true
+                            )})?`,
+                            fields: [
+                                {
+                                    name:
+                                        options.type === "player"
+                                            ? `Punishment Received (ID: ${options.punishmentID})`
+                                            : `Punishment Given (ID: ${options.punishmentID})`,
+                                    value: [
+                                        `\`\`\`Type: ${type}`,
+                                        type.includes("GLOBAL")
+                                            ? undefined
+                                            : `Server: ${server}`,
+                                        `Platform: ${
+                                            parsePlayerID(punishedPlayerID)
+                                                .platform
+                                        }`,
+                                        options.type === "admin"
+                                            ? `Player: ${
+                                                  punishedPlayer.name
+                                              } (${outputPlayerIDs(
+                                                  punishedPlayer.ids
+                                              )})`
+                                            : undefined,
+                                        `Date: ${new Date(
+                                            date
+                                        ).toDateString()} (${formatDistanceToNow(
+                                            date,
+                                            { addSuffix: true }
+                                        )})`,
+                                        `Offense: ${reason || "None given"}`,
+                                        [
+                                            "BAN",
+                                            "MUTE",
+                                            "GLOBAL BAN",
+                                            "GLOBAL MUTE",
+                                        ].includes(type)
+                                            ? `Duration: ${
+                                                  !duration
+                                                      ? "PERMANENT"
+                                                      : pluralize(
+                                                            "minute",
+                                                            duration,
+                                                            true
+                                                        )
+                                              } ${
+                                                  duration
+                                                      ? `(Un${
+                                                            type === "BAN"
+                                                                ? "banned"
+                                                                : "muted"
+                                                        } ${formatDistanceToNow(
+                                                            addMinutes(
+                                                                date,
+                                                                duration
+                                                            ),
+                                                            {
+                                                                addSuffix: true,
+                                                            }
+                                                        )})`
+                                                      : ""
+                                              }`
+                                            : undefined,
+                                        `Admin: ${admin}\`\`\``,
+                                    ]
+                                        .filter(
+                                            (line) =>
+                                                typeof line !== "undefined"
+                                        )
+                                        .join("\n"),
+                                },
+                            ],
+                            color: 15158332,
+                        },
+                    ],
+                },
+                async (btnCtx) => {
+                    if (ctx.user.id !== btnCtx.user.id) return;
+
+                    console.log("pressed");
+                }
+            );
+        } catch (error) {
+            await ctx.send({
+                content: `An error occured while performing the command (${
+                    error.message || error
+                })`,
+            });
+        }
+    }
+}
