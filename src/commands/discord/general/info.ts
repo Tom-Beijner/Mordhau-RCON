21ff756d959ab7e53b7cbfd240619ddca6e91831
Tom Beijner <tom.beijner@gmail.com>
2021-11-03 17:43:28 +0100

    refactor(info): add git commit version to the embed

--- src/commands/discord/general/info.ts
+++ src/commands/discord/general/info.ts
@@ -1,8 +1,12 @@
+import { exec } from "child_process";
 import pluralize from "pluralize";
 import { CommandContext, SlashCreator } from "slash-create";
+import { promisify } from "util";
 import DiscordEmbed from "../../../structures/DiscordEmbed";
 import SlashCommand from "../../../structures/SlashCommand";
 import Watchdog from "../../../structures/Watchdog";
+
+const execPromise = promisify(exec);
 
 export default class Info extends SlashCommand {
     constructor(creator: SlashCreator, bot: Watchdog, commandName: string) {
@@ -23,17 +27,35 @@ export default class Info extends SlashCommand {
                 );
             }
 
+            let gitCommit: string;
+            try {
+                gitCommit = (
+                    (await execPromise("git rev-parse HEAD")).stdout as string
+                ).trim();
+            } catch {}
+
             const embed = new DiscordEmbed();
 
             embed
                 .setTitle("Information")
                 .setDescription(
-                    "This bot is open source, available on [GitHub](https://github.com/Tom-Beijner/Mordhau-RCON)"
+                    "This bot is open source, available on [GitHub](https://github.com/Tom-Beijner/Mordhau-RCON)\n\nMade by [Tom Beijner AKA Schweppes](https://tombeijner.com/)"
                 )
                 .addField(
                     "Versions",
                     [
-                        `Bot: [v${process.env.npm_package_version}](https://github.com/Tom-Beijner/Mordhau-RCON/releases/tag/${process.env.npm_package_version})`,
+                        `Bot: [v${
+                            process.env.npm_package_version
+                        }](https://github.com/Tom-Beijner/Mordhau-RCON/releases/tag/${
+                            process.env.npm_package_version
+                        }) (${
+                            gitCommit
+                                ? `[${gitCommit.substring(
+                                      0,
+                                      7
+                                  )}](https://github.com/Tom-Beijner/Mordhau-RCON/commit/${gitCommit})`
+                                : "Unknown"
+                        })`,
                         `Node.JS: [${process.version}](https://nodejs.org/en/)`,
                     ].join("\n"),
                     true
